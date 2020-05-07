75c48d344c5e7f5fa4c2b8d2a7b6cdd9689dd9f5
UniversalFlamer <36077293+UniversalFlamer@users.noreply.github.com>
2020-05-08 02:32:02 +0800

    initialization

---
+++ src/main/java/liyuxuan/bank_system/BankSystemApplication.java
@@ -0,0 +1,15 @@
+package liyuxuan.bank_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BankSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/liyuxuan/bank_system/BankSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package liyuxuan.bank_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
